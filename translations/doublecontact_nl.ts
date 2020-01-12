4de73b15ca247cb407a4212af709796a70556e76
Heimen Stoffels <vistausss@outlook.com>
2020-01-12 11:39:29 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (418 of 418 strings)
    
    Translation: DoubleContact/Translations
    Translate-URL: https://hosted.weblate.org/projects/doublecontact/translations/nl/

--- translations/doublecontact_nl.ts
+++ translations/doublecontact_nl.ts
@@ -2133,7 +2133,8 @@ Controleer gebruikersnaam, wachtwoord, protocol en poort</translation>
         <location filename="../core/decodedmessagelist.cpp" line="49"/>
         <source>&quot;Date&quot;,&quot;Box&quot;,&quot;From/To&quot;,&quot;Status&quot;,&quot;Text&quot;,&quot;Aux&quot;
 </source>
-        <translation type="unfinished"></translation>
+        <translation>&quot;Date&quot;,&quot;Box&quot;,&quot;From/To&quot;,&quot;Status&quot;,&quot;Text&quot;,&quot;Aux&quot;
+</translation>
     </message>
     <message>
         <location filename="../core/decodedmessagelist.cpp" line="54"/>
