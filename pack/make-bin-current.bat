0d082926b89d8d048987726e6a2de6dc66f53247
Mikhail Zvyozdochkin <pub@zvyozdochkin.ru>
2020-12-16 03:39:57 +0300

    Fixed script for portable version make@

--- pack/make-bin-current.bat
+++ pack/make-bin-current.bat
@@ -2,4 +2,4 @@ set PKG_NAME=doublecontact_current_win32_portable
 set PKG_DIR=..\%PKG_NAME%
 
 call .\make-bin-image.bat .. ..\..\build-all-Qt_4_8_6_st-Release C:\Qt\2009.05\mingw C:\Qt\4.8.6.st %PKG_DIR%
-copy doublecontact.ini %PKG_DIR%\doublecontact.ini
+copy .\zip\doublecontact.ini %PKG_DIR%\doublecontact.ini
