DarkHobbit/doublecontact

commit d85431b320380fc2c56ef538e471763b0f234a8e

Sort column and order now stored for each table

app/helpers.cpp
@@ -15,6 +15,7 @@
 #include <QHeaderView>
 #include <QMessageBox>
 #include <QPixmap>
+#include <QSortFilterProxyModel>
 
 #include "globals.h"
 #include "helpers.h"
@@ -67,3 +68,36 @@ void updateTableConfig(QTableView *table)
     }
 }
 
+void readTableSortConfig(QTableView *table, bool forceNeedSort, bool needSort)
+{
+    bool enabled;
+    int column;
+    Qt::SortOrder order;
+    configManager.readSortConfig(table->objectName(), enabled, column, order);
+    if (forceNeedSort)
+        enabled = needSort;
+    if (!enabled)
+        column = -1;
+    table->setSortingEnabled(enabled);
+    if (enabled)
+        table->horizontalHeader()->setSortIndicator(column, order);
+}
+
+void writeTableSortConfig(QTableView *table)
+{
+    QHeaderView *header = table->horizontalHeader();
+    configManager.writeSortConfig(table->objectName(),
+           table->isSortingEnabled(),
+           header->sortIndicatorSection(),
+           header->sortIndicatorOrder());
+}
+
+void writeTableSortConfig(QHeaderView* header)
+{
+    QTableView* table = dynamic_cast<QTableView*>(header->parent());
+    if (table)
+        configManager.writeSortConfig(table->objectName(),
+               table->isSortingEnabled(),
+               header->sortIndicatorSection(),
+               header->sortIndicatorOrder());
+}

app/helpers.h
@@ -14,10 +14,13 @@
 #ifndef HELPERS_H
 #define HELPERS_H
 
+#include <QtGlobal>
+#include <QHeaderView>
 #include <QItemDelegate>
 #include <QLabel>
 #include <QTableView>
 
+#include "configmanager.h"
 #include "contactlist.h"
 
 // Delegate for making table widgets read only
@@ -39,5 +42,9 @@ void showPhoto(const Photo& photo, QLabel* label);
 // Set color/font for each table view
 void updateTableConfig(QTableView* table);
 
+// Table view sort settings
+void readTableSortConfig(QTableView* table, bool forceNeedSort, bool needSort = false);
+void writeTableSortConfig(QTableView* table);
+void writeTableSortConfig(QHeaderView* header);
 
 #endif // HELPERS_H

app/mainwindow.cpp
@@ -82,9 +82,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->tvLeft->setModel(proxyLeft);
     ui->tvRight->setModel(proxyRight);
     connect(modLeft, SIGNAL(requestCSVProfile(CSVFile*)), this, SLOT(onRequestCSVProfile(CSVFile*)), Qt::DirectConnection);
+    connect(modRight, SIGNAL(requestCSVProfile(CSVFile*)), this, SLOT(onRequestCSVProfile(CSVFile*)), Qt::DirectConnection);
     ui->tvLeft->horizontalHeader()->setStretchLastSection(true);
     ui->tvRight->horizontalHeader()->setStretchLastSection(true);
-    // Status bar
+    connect(ui->tvLeft->horizontalHeader(), SIGNAL(sortIndicatorChanged(int,Qt::SortOrder)), this, SLOT(onSortIndicatorChanged(int,Qt::SortOrder)));
+    connect(ui->tvRight->horizontalHeader(), SIGNAL(sortIndicatorChanged(int,Qt::SortOrder)), this, SLOT(onSortIndicatorChanged(int,Qt::SortOrder)));
+     // Status bar
     lbCount = new QLabel(0);
     lbMode = new QLabel(0);
     statusBar()->addWidget(lbCount);
@@ -126,8 +129,9 @@ MainWindow::MainWindow(QWidget *parent) :
         open(selectedModel, configManager.lastContactFile(), ftAuto);
     // Show data
     ui->action_Two_panels->setChecked(configManager.showTwoPanels());
-    ui->action_Sort->setChecked(configManager.sortingEnabled());
-    setSorting(configManager.sortingEnabled());
+    readTableSortConfig(ui->tvLeft, false);
+    readTableSortConfig(ui->tvRight, false);
+    ui->action_Sort->setChecked(ui->tvLeft->isSortingEnabled());
     updateHeaders();
     updateModeStatus();
     updateRecent();
@@ -491,9 +495,16 @@ void MainWindow::on_btnCompare_clicked()
 // Sort List
 void MainWindow::on_action_Sort_toggled(bool needSort)
 {
-    setSorting(needSort);
-    configManager.setSortingEnabled(needSort);
+    readTableSortConfig(selectedView, true, needSort);
+    writeTableSortConfig(selectedView);
     updateModeStatus();
+}
+
+void MainWindow::onSortIndicatorChanged(int, Qt::SortOrder)
+{
+    QHeaderView* header = dynamic_cast<QHeaderView*>(sender());
+    if (header)
+        writeTableSortConfig(header);
 }
 
 void MainWindow::on_btnSort_clicked()
@@ -527,15 +538,6 @@ bool MainWindow::checkSelection(bool errorIfNoSelected, bool onlyOneRowAllowed)
     foreach(QModelIndex index, proxySelection)
         selection << selectedProxy->mapToSource(index);
     return true;
-}
-
-void MainWindow::setSorting(bool needSort)
-{
-    ui->tvLeft->setSortingEnabled(needSort);
-    ui->tvRight->setSortingEnabled(needSort);
-    int sortColumn = needSort ? 0 : -1;
-    proxyLeft->sort(sortColumn);
-    proxyRight->sort(sortColumn);
 }
 
 void MainWindow::updateListHeader(ContactModel *model, QLabel *header)
@@ -572,6 +574,7 @@ void MainWindow::setButtonsAccess()
     ui->btnEdit->setEnabled(hasSelectedRows);
     ui->btnRemove->setEnabled(hasSelectedRows);
     ui->btnSwapNames->setEnabled(hasSelectedRows);
+    ui->action_Sort->setChecked(selectedView->isSortingEnabled());
 }
 
 void MainWindow::selectionChanged()

app/mainwindow.h
@@ -67,6 +67,7 @@ private slots:
     void on_tvLeft_clicked(const QModelIndex&);
     void on_tvRight_clicked(const QModelIndex&);
     void on_action_Sort_toggled(bool needSort);
+    void onSortIndicatorChanged(int,Qt::SortOrder);
     void on_btnSort_clicked();
     void on_action_OpenFile_triggered();
     void on_action_OpenDir_triggered();
@@ -117,7 +118,6 @@ private:
     void buildContextMenu(QTableView* view);
     void selectView(QTableView* view);
     bool checkSelection(bool errorIfNoSelected = true, bool onlyOneRowAllowed = false);
-    void setSorting(bool needSort);
     void updateListHeader(ContactModel* model, QLabel* header);
     void updateHeaders();
     void updateModeStatus();

app/messagewindow.cpp
@@ -74,7 +74,7 @@ MessageWindow::MessageWindow(ContactList* contacts) :
     proxy->setSortRole(SortStringRole);
     proxy->setFilterCaseSensitivity(Qt::CaseInsensitive); // Driver == driver
     ui->tvMessages->setModel(proxy);
-    ui->tvMessages->setSortingEnabled(configManager.sortingEnabled());
+    connect(ui->tvMessages->horizontalHeader(), SIGNAL(sortIndicatorChanged(int,Qt::SortOrder)), this, SLOT(onSortIndicatorChanged(int,Qt::SortOrder)));
     connect(ui->tvMessages->selectionModel(), SIGNAL(currentChanged(QItemIndex,QItemIndex)),
             this, SLOT(selectionChanged()));
     connect(ui->tvMessages->selectionModel(), SIGNAL(selectionChanged(QItemSelection,QItemSelection)),
@@ -93,7 +93,7 @@ MessageWindow::MessageWindow(ContactList* contacts) :
     // Calc!
     updateModel();
     ui->tvMessages->resizeColumnsToContents();
-    setSorting(configManager.sortingEnabled());
+    readTableSortConfig(ui->tvMessages, false);
 }
 
 MessageWindow::~MessageWindow()
@@ -241,7 +241,16 @@ void MessageWindow::updateStatus()
 void MessageWindow::toggleSort()
 {
     bool needSort = !ui->tvMessages->isSortingEnabled();
-    setSorting(needSort);
+    readTableSortConfig(ui->tvMessages, true, needSort);
+    writeTableSortConfig(ui->tvMessages);
+    updateStatus();
+}
+
+void MessageWindow::onSortIndicatorChanged(int, Qt::SortOrder)
+{
+    QHeaderView* header = dynamic_cast<QHeaderView*>(sender());
+    if (header)
+        writeTableSortConfig(header);
 }
 
 void MessageWindow::on_actionCopy_text_triggered()
@@ -318,14 +327,6 @@ void MessageWindow::showEvent(QShowEvent *)
 {
     ui->tvMessages->resizeRowsToContents();
     ui->tvMessages->selectRow(0);
-}
-
-void MessageWindow::setSorting(bool needSort)
-{
-    ui->tvMessages->setSortingEnabled(needSort);
-    int sortColumn = needSort ? 0 : -1;
-    proxy->sort(sortColumn);
-    updateStatus();
 }
 
 void MessageWindow::on_actionSave_MMS_Files_triggered()

app/messagewindow.h
@@ -45,6 +45,7 @@ private slots:
     void on_cbMergeDups_stateChanged(int);
     void on_cbMergeMultiparts_stateChanged(int);
     void toggleSort();
+    void onSortIndicatorChanged(int,Qt::SortOrder);
     void on_actionCopy_text_triggered();
     void on_actionProperties_triggered();
     void on_leFilter_textChanged(const QString &newText);
@@ -65,7 +66,6 @@ private:
     QStatusBar* statusBar;
     QLabel *lbCount, *lbMMSCount, *lbMode, *lbDups, *lbMultiParts;
     QMenu* menuMMSFiles;
-    void setSorting(bool needSort);
     void updateModel();
     void updateStatus();
     void checkButtons();

model/configmanager.cpp
@@ -283,18 +283,28 @@ void ConfigManager::readEditResolution(int &width, int &height)
     height = settings->value("Edit/Height", 0).toInt();
 }
 
-bool ConfigManager::sortingEnabled()
-{
-    if (!settings)
-        return false;
-    return settings->value("General/SortingEnabled", false).toBool();
-}
-
-void ConfigManager::setSortingEnabled(bool value)
+void ConfigManager::readSortConfig(const QString& tableName,
+                                      bool& enabled, int& column, Qt::SortOrder& order)
 {
     if (!settings)
         return;
-    settings->setValue("General/SortingEnabled", value);
+    //    return settings->value("General/SortingEnabled", false).toBool();
+    QString section = QString("Tables/%1/").arg(tableName);
+    enabled = settings->value(section+"SortingEnabled", true).toBool();
+    column = settings->value(section+"SortColumn", 0).toInt();
+    int iOrder = settings->value(section+"SortOrder", Qt::AscendingOrder).toInt();
+    order = (iOrder==0) ? Qt::AscendingOrder : Qt::DescendingOrder;
+}
+
+void ConfigManager::writeSortConfig(const QString& tableName,
+                                       bool enabled, int column, Qt::SortOrder order)
+{
+    if (!settings)
+        return;
+    QString section = QString("Tables/%1/").arg(tableName);
+    settings->setValue(section+"SortingEnabled", enabled);
+    settings->setValue(section+"SortColumn", column);
+    settings->setValue(section+"SortOrder", order);
 }
 
 ContactList::SortType ConfigManager::hardSortType()

model/configmanager.h
@@ -14,6 +14,7 @@
 #ifndef CONFIGMANAGER_H
 #define CONFIGMANAGER_H
 
+#include <QtGlobal>
 #include <QSettings>
 #include <QStringList>
 
@@ -53,8 +54,10 @@ public:
         int& addrCount, QStringList& addrTypes,
         int& width, int& height);
     void readEditResolution(int& width, int& height);
-    bool sortingEnabled();
-    void setSortingEnabled(bool value);
+    void readSortConfig(const QString& tableName,
+        bool& enabled, int& column, Qt::SortOrder& order);
+    void writeSortConfig(const QString& tableName,
+        bool enabled, int column, Qt::SortOrder order);
     ContactList::SortType hardSortType();
     void setHardSortType(ContactList::SortType sortType);
     void csvConfig(QString& profile, QString& genEncoding, QString& genSeparator);

commit 16597c22ea8685cdb9fc3abc2c846a704e32f9e5

Fixed: switching sort off was broken in d85431b

app/helpers.cpp
@@ -81,6 +81,10 @@ void readTableSortConfig(QTableView *table, bool forceNeedSort, bool needSort)
     table->setSortingEnabled(enabled);
     if (enabled)
         table->horizontalHeader()->setSortIndicator(column, order);
+    QSortFilterProxyModel* model =
+            dynamic_cast<QSortFilterProxyModel*>(table->model());
+    if (model)
+        model->sort(column);
 }
 
 void writeTableSortConfig(QTableView *table)